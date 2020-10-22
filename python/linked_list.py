512f77010eb4bffe268d9183e25f417421c084b2
dylansouthwick <dylansouthwick.ds@gmail.com>
2020-10-22 16:43:52 -0400

    linkedlist before remove

--- python/linked_list.py
+++ python/linked_list.py
@@ -1,19 +1,50 @@
 class LinkList:
   # write your __init__ method here that should store a 'head' value which the first Node in the LinkedList and a 'length' value which is the total number of Nodes in the LinkedList
+  def __init__(self,data):
+    self.head = Node(data)
 
   def add(self, data):
-    # write your code to ADD an element to the Linked List
-    pass
+    current = self.head
+    while current.next is not None:
+      current = current.next
+    current.next = Node(data)
 
   def remove(self, data):
     # write your code to REMOVE an element from the Linked List
     pass
 
   def get(self, element_to_get):
-    # write you code to GET and return an element from the Linked List
-    pass
+    # 1 2 3 4
+    # target : 5
+
+    current = self.head
+    while current is not None and current.data != element_to_get:
+      # current = Node(4)
+      current = current.next # current = None
+    
+    return current if current else False
+
+  def __str__(self):
+    answer = []
+    current = self.head
+    while current is not None:
+      answer.append(current.data)
+      current = current.next
+    return str(answer)
 
 # ----- Node ------
 class Node:
   # store your DATA and NEXT values here
-  pass
+  def __init__(self,data,next = None):
+    self.data = data
+    self.next = next
+
+
+ll = LinkList(0)
+ll.add(1)
+ll.add(2)
+ll.add(3)
+ll.add(4)
+print(ll)
+print('getting 2:',ll.get(2).data)
+print('getting 5: ',ll.get(5))
--- python/stack.py
+++ python/stack.py
@@ -1,14 +1,25 @@
 class Stack:
   # write your __init__ method here that should store a 'total' value which is the total number of elements in the Stack and a 'stack' value which is an array of stored values in the Stack
+  def __init__(self):
+    self.container = []
 
-  def push(self):
-    # write your code to add data following LIFO and return the Stack
-    pass
+  def push(self,value):
+    self.container.append(value)
 
-  def pop(self, data):
-    # write your code to removes the data following LIFO and return the Stack
-    pass
+  def pop(self):
+    return self.container.pop()
 
   def size(self):
-    # write your code that returns the size of the Stack
-    pass
+    return len(self.container)
+  
+  def __str__(self):
+    return str(self.container)
+
+stack = Stack()
+stack.push(1)
+stack.push(2)
+stack.push(3)
+print(stack) # 123
+
+print('popped last one', stack.pop()) #3
+print(stack) #12
